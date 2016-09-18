084b097a86f5472d4d16e46cc6157af639486a94
SongFuZhen <evil_song@126.com>
2016-09-18 10:13:39 +0800

    nav changed

--- oee-server/app/assets/javascripts/layout.js
+++ oee-server/app/assets/javascripts/layout.js
@@ -13,20 +13,23 @@ layout.init = function () {
             PageAction('#users', '新增用户', '编辑用户', '创建', '更新');
             break;
         case "crafts":
+            $('.nav-basic').addClass('nav-active');
             $('.nav-crafts').addClass('nav-active');
             break;
         case "departments":
+            $('.nav-basic').addClass('nav-active');
             $('.nav-departments').addClass('nav-active');
             break;
         case "downtime_codes":
-            $('.nav-downtime').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-downtime_codes').addClass('nav-active');
             break;
         case "downtime_types":
-            $('.nav-downtime').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-downtime_types').addClass('nav-active');
             break;
         case "downtime_records":
+            $('.nav-oee').addClass('nav-active');
             $('.nav-downtime').addClass('nav-active');
             if (pathname[2] == "display") {
                 $('.nav-display_downtime_records').addClass('nav-active');
@@ -35,23 +38,23 @@ layout.init = function () {
             }
             break;
         case "work_times":
-            $('.nav-machine').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-work_times').addClass('nav-active');
             break;
         case "machine_types":
-            $('.nav-machine').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-machine_types').addClass('nav-active');
             break;
         case "machines":
-            $('.nav-machine').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-machines').addClass('nav-active');
             break;
         case "work_shifts":
-            $('.nav-settings').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-work_shifts').addClass('nav-active');
             break;
         case "holidays":
-            $('.nav-settings').addClass('nav-active');
+            $('.nav-basic').addClass('nav-active');
             $('.nav-holidays').addClass('nav-active');
             break;
         default:
